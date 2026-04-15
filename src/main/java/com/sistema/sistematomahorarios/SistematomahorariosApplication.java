2f221227cdb406964647effb9bf61ba9472c21c4
GoNNav7w7 <gon.navarrete@duocuc.cl>
2026-04-15 12:13:18 -0400

    Initial Commit

---
+++ src/main/java/com/sistema/sistematomahorarios/SistematomahorariosApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.sistematomahorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistematomahorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistematomahorariosApplication.class, args);
+	}
+
+}
