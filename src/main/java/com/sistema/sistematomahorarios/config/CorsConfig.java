a23108ff0c2f54848c7f98a939595305829cd48e
Carla Prado <ca.pradom@duocuc.cl>
2026-06-18 03:57:08 -0400

    update cors

--- src/main/java/com/sistema/sistematomahorarios/config/CorsConfig.java
+++ src/main/java/com/sistema/sistematomahorarios/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://taller-aplicado-programacion-frontend-5m8ur3n5v.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
